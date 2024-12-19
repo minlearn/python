552ad8a227eacbeaadd9016cd3405fec849f26f1
minlearnminlearn <minlearn@minlearn.org>
2024-12-20 00:47:52 +0800

    Update python_install.sh

--- python_install.sh
+++ python_install.sh
@@ -1,19 +1,19 @@
 ###############
 
+silent() { "$@" >/dev/null 2>&1; }
+
 echo "Installing Dependencies"
-apt-get install -y curl
-apt-get install -y sudo
-apt-get install -y mc
+silent apt-get install -y curl sudo mc
 echo "Installed Dependencies"
 
 echo "Installing Python Dependencies"
-apt-get -y install python3-pip
+silent apt-get -y install python3-pip
 rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED
 echo "Installed Python Dependencies"
 
 echo "Cleaning up"
-apt-get -y autoremove
-apt-get -y autoclean
+silent apt-get -y autoremove
+silent apt-get -y autoclean
 echo "Cleaned"
 
 ##############
